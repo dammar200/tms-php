eab2a46f332defe9033f00d33b4d7a5602e166fb
dammar200 <dammarkhadayat.xdezo@gmail.com>
2023-06-22 10:51:34 +0545

    add new new file login

--- index.php
+++ index.php
@@ -1,109 +1,32 @@
 <!doctype html>
 <html lang="en">
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <title>Title</title>
 </head>
-
 <body>
-    <nav class="container navbar navbar-expand-lg navbar-light bg-light shadow">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
-                    </li>
-                    <!-- <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li> -->
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Add
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="users/create.php">Add user</a></li>
-                    </ul>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Manage
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="users/index.php">Manage User</a></li>
-                    </ul>
-                </li>
-                </ul>
-                <form class="d-flex">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
-            </div>
-        </div>
-    </nav>
 
-    <section>
-        <div class="row container mx-auto my-5">
-            <div class="col-lg-3 py-2">
-                <div class="card">
-                    <a href="users/index.php" class="text-decoration-none">
-                        <div class="card-body text-center">
-                            <h5 class="card-title">Users </h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="col-lg-3 py-2">
-                <div class="card">
-                    <a href="users/index.php" class="text-decoration-none">
-                        <div class="card-body text-center">
-                            <h5 class="card-title">Users </h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="col-lg-3 py-2">
-                <div class="card">
-                    <a href="users/index.php" class="text-decoration-none">
-                        <div class="card-body text-center">
-                            <h5 class="card-title">Users </h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="col-lg-3 py-2">
-                <div class="card">
-                    <a href="users/index.php" class="text-decoration-none">
-                        <div class="card-body text-center">
-                            <h5 class="card-title">Users </h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="col-lg-3 py-2">
-                <div class="card">
-                    <a href="users/index.php" class="text-decoration-none">
-                        <div class="card-body text-center">
-                            <h5 class="card-title">Users </h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                    </a>
-                </div>
-            </div>
-
+<div class="container w-25 mx-auto  py-5">
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">Login</h5>
+            <form>
+              <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+              </div>
+              <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1">
+              </div>
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
         </div>
-    </section>
+    </div>
+</div>
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 </body>
-
 </html>
